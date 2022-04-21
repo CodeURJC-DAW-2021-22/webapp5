0456652995260dbf8dcfed84ce10e411e1a1389c
JEstebanPerez <j.esteban.2019@alumnos.urjc.es>
2022-04-21 02:59:10 +0200

    Angular added

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { HeaderComponent } from './header.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
