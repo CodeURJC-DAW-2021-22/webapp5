1b897672401d597c076c7ed035f0a5fadbef6299
pablo.pinillos09@gmail.com <pablo.pinillos09@gmail.com>
2022-04-25 17:51:09 +0200

    folders created and about added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,21 +1,16 @@
-<<<<<<< .merge_file_a31928
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-=======
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './index.component';
-import { LoginComponent } from './login.component';
+import { IndexComponent } from './components/index.component';
+import { LoginComponent } from './components/login.component';
+import { AboutComponent } from './components/about.component';
 
 schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 
 const routes: Routes = [
   {path: '', component: IndexComponent},
-  {path: 'login', component: LoginComponent}];
+  {path: 'login', component: LoginComponent},
+  {path: 'about', component: AboutComponent}];
 
->>>>>>> .merge_file_a19140
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { LoginComponent } from './login.component';
-import { FooterComponent } from './footer.component';
-import { HeaderComponent } from './header.component';
+import { LoginComponent } from './components/login.component';
+import { FooterComponent } from './components/footer.component';
+import { HeaderComponent } from './components/header.component';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { IndexComponent } from './index.component';
-import { AdminHeaderComponent } from './adminHeader.component';
+import { IndexComponent } from './components/index.component';
+import { AdminHeaderComponent } from './components/adminHeader.component';
+import { AboutComponent } from './components/about.component';
 
 schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, AdminHeaderComponent, FooterComponent, IndexComponent, LoginComponent
+    AppComponent, HeaderComponent, AdminHeaderComponent, FooterComponent, IndexComponent, LoginComponent, AboutComponent
   ],
   imports: [
     BrowserModule,
