54fcec512aabd8fa72dd64147617fb32c340bc5a
a-sanchezgu-2019 <a.sanchezgu.2019@alumnos.urjc.es>
2022-05-03 04:09:55 +0200

    Out of stock page updated

--- frontend/src/app/components/cart/out-of-stock.component.ts
+++ frontend/src/app/components/cart/out-of-stock.component.ts
@@ -1,3 +1,4 @@
+import { ProductsService } from './../../services/products.service';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,12 +10,21 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class OutOfStockComponent {
 
   productId: number;
+  productName: string;
+  productStock: number;
 
-  constructor(private router: Router, activatedRoute: ActivatedRoute, private httpClient: HttpClient) {
+  constructor(private router: Router, activatedRoute: ActivatedRoute, private httpClient: HttpClient, private productsService: ProductsService) {
     this.httpClient.get("/api/users/me", {withCredentials: true}).subscribe({
       error: _ => this.router.navigate(['/login'])
     });
     this.productId = activatedRoute.snapshot.params['id'];
+    this.productsService.getProduct(this.productId).subscribe({
+      next: product => {
+        this.productName = product.name;
+        this.productStock = product.stock;
+      },
+      error: error => console.error(error)
+    })
   }
 
 }
